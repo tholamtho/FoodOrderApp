333ee532b20f68237ffd27412a5da9b0118674d7
tholamtho <lamtho1999@gmail.com>
2022-06-17 15:40:09 +0700

    api testing

--- back-end/app.ts
+++ back-end/app.ts
@@ -1,9 +1,37 @@
-const express = require('express');
-
+const express = require("express");
+import { Products } from "./interface";
+require("dotenv/config");
 const app = express();
 
-app.listen(3000, ()=> {
-    //receive from port 3000
-    console.log('Server on!');
-    
+let api = process.env.API_URL;
+
+app.get("/", (req: any, res: any) => {
+  res.send("Main page");
+});
+
+app.get(`${api}/products`, (req: any, res: any) => {
+  const products: Products = {
+    id: "1",
+    name: "food-no-1",
+    price: 500,
+  };
+
+  res.send(products);
+});
+
+app.get(`${api}/providers`, (req: any, res: any) => {
+  res.send("Providers page");
+});
+
+app.get(`${api}/customers`, (req: any, res: any) => {
+  res.send("Customers page");
+});
+
+app.get(`${api}/admin`, (req: any, res: any) => {
+  res.send("Admin page");
+});
+
+app.listen(3000, () => {
+  //receive from port 3000
+  console.log("Server on!");
 });
---
+++ back-end/interface.ts
@@ -0,0 +1,6 @@
+export interface Products {
+  id: String;
+  name: String;
+  price: Number;
+  image?: String;
+}
