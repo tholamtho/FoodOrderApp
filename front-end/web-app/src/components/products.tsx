9e36803150c0b8a8e4465e938f390350f44c6ed1
tholamtho <lamtho1999@gmail.com>
2022-07-04 15:47:44 +0700

    add react router

---
+++ front-end/web-app/src/components/products.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Products extends Component {
+  render() {
+    return (
+      <>
+        <h2>products-name</h2>
+        <img src="image-url" />
+        <span> $ price-of-product</span>
+      </>
+    );
+  }
+}
---
+++ front-end/web-app/src/screens/CustomerScreen.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class CustomerScreen extends Component {
+  render() {
+    return <div>CustomerScreen</div>;
+  }
+}
