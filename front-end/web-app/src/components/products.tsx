6a943988cbfe29efc94d7ad2add79e9208aff1f2
tholamtho <lamtho1999@gmail.com>
2022-07-17 01:34:05 +0700

    update

--- front-end/web-app/src/components/products.tsx
+++ front-end/web-app/src/components/products.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { Card } from "react-bootstrap";
 import ProductInterface from "../interfaces/products";
 import { handleClickAddToCard } from "../utils/addToCart";
@@ -8,13 +8,15 @@ export const Products: React.FC<ProductInterface> = (
 ) => {
   return (
     <>
-    <Card className="my-3 p3 rounded">
-      <a href={`/products/${product.id}`}>
-        <Card.Img src={product.imageSource} />
-        <h2>{product.name}</h2>
-      </a>
-    <Button variant="primary" onClick={handleClickAddToCard(product.name)}>Add To Cart</Button>
-    </Card>
+      <Card className="my-3 p3 rounded">
+        <a href={`/products/${product.id}`}>
+          <Card.Img src={product.imageSource} />
+          <h2>{product.name}</h2>
+        </a>
+        <Button variant="primary" onClick={handleClickAddToCard(product.name)}>
+          Add To Cart
+        </Button>
+      </Card>
     </>
   );
 };
