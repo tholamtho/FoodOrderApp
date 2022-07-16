8e0a23ec08eb74ab393eda4b3953d9b27103ef1f
tholamtho <lamtho1999@gmail.com>
2022-07-17 04:02:43 +0700

    add Rating

--- front-end/web-app/src/components/Products.tsx
+++ front-end/web-app/src/components/Products.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import { Card } from "react-bootstrap";
 import ProductInterface from "../interfaces/products";
 import { handleClickAddToCard } from "../utils/addToCart";
+import { Rating } from "react-simple-star-rating";
 export const Products: React.FC<ProductInterface> = (
   product: ProductInterface
 ) => {
+  const [ratingValue, setRatingValue] = useState(product.rated);
+
+  const handleRating = (rate: number) => {
+    setRatingValue(rate);
+  };
+
+  // later in your code call rating 'state' with your initial value
+  const handleReset = () => {
+    setRatingValue(2.5);
+  };
   return (
     <>
       <Card
@@ -19,6 +30,11 @@ export const Products: React.FC<ProductInterface> = (
           />
           <h2>{product.name}</h2>
         </a>
+        <Rating
+          onClick={handleRating}
+          initialValue={5}
+          ratingValue={product.rated}
+        />
         <p style={{ overflow: "hidden" }}>Product Details: {product.details}</p>
         <Button variant="primary">Add To Cart</Button>
       </Card>
