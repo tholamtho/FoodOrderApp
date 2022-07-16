c10fd772f6888a0646745f2357a7879533624fcd
tholamtho <lamtho1999@gmail.com>
2022-07-17 01:22:01 +0700

    update product screen

--- front-end/web-app/src/components/products.tsx
+++ front-end/web-app/src/components/products.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 import { Card } from "react-bootstrap";
 import ProductInterface from "../interfaces/products";
 export const Products: React.FC<ProductInterface> = (
   product: ProductInterface
 ) => {
+  const handleClickAddToCard :any = (name: string) => {
+    alert(`Added ${name} to card successfully`);
+  }
   return (
+    <>
     <Card className="my-3 p3 rounded">
       <a href={`/products/${product.id}`}>
         <Card.Img src={product.imageSource} />
         <h2>{product.name}</h2>
       </a>
+    <Button variant="primary" onClick={handleClickAddToCard(product.name)}>Add To Cart</Button>
     </Card>
+    </>
   );
 };
